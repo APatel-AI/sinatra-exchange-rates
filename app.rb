9249027f91c4e31974bc7588275a5134051306fd
Arpan Patel <113603155+APatel-AI@users.noreply.github.com>
2024-02-20 19:31:56 +0000

    added initial template layout for routes and created the root route

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
+require "http"
+require "json"
 require "sinatra"
 require "sinatra/reloader"
 
+# Available Currencies
+api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+raw_api = HTTP.get(api_url)
+parsed_api = JSON.parse(raw_api)
+raw_currencies =  parsed_api.fetch("currencies")
+
+@currencies = raw_currencies.values
+@currency_abbreviation = raw_currencies.keys
+
+# Home Screen
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  raw_api = HTTP.get(api_url)
+  parsed_api = JSON.parse(raw_api)
+  raw_currencies =  parsed_api.fetch("currencies")
+  @currencies = raw_currencies.values
+  @currency_abbreviation = raw_currencies.keys
+ erb(:homepage)
 end
