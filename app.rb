42554ba5d5c60827cc3f309c1a3e76f1d55d4574
Arpan Patel <113603155+APatel-AI@users.noreply.github.com>
2024-02-20 20:15:00 +0000

    added conversion route and updated the html format

--- app.rb
+++ app.rb
@@ -13,8 +13,7 @@ raw_currencies =  parsed_api.fetch("currencies")
 @currencies = raw_currencies.values
 @currency_abbreviation = raw_currencies.keys
 
-# Home Screen
-
+# Hompage.erb (ROOT ROUTE)
 get("/") do
   api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
 
@@ -32,4 +31,29 @@ get("/:currency_one") do
   @currency_abbreviation = raw_currencies.keys
 
   erb(:currency_template)
+end
+
+get ("/:currency_one/:currency_two") do
+  #initial currency that needs to be converted
+  currency_from = params.fetch("currency_one")
+  
+  #the currency we want to convert to
+  currency_to = params.fetch("currency_two")
+  
+  #default value 1
+  amount ="1"
+
+  #conversion api url
+  conversion_url = "http://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{currency_from}&to=#{currency_to}&amount=#{amount}"
+
+  #to json
+  raw_conversion_api = HTTP.get(conversion_url)
+  parsed_conversion_api = JSON.parse(raw_conversion_api)
+
+
+  @converted = parsed_conversion_api.fetch("result")
+  @currencies = raw_currencies.values
+  @currency_abbreviation = raw_currencies.keys
+
+  erb(:conversion)
 end
